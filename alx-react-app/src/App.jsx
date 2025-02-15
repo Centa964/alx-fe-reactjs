d5eda6e605b4593c386ff488763d228802f4111e
Mwende Nzau <ancentamwende@gmail.com>
2025-02-15 12:17:44 +0300

    added userprofile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,7 @@ import './App.css'
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from "./components/UserProfile";
 
 
 function App() {
@@ -40,6 +41,9 @@ function App() {
         <Header />
         <MainContent />
         <Footer />
+      </div>
+      <div>
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       </div>
     </>
   )
