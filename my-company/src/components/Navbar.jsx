8de2ec01cd7b1fbd8ce21f80179e9e1cfbf59e0b
Mwende Nzau <ancentamwende@gmail.com>
2025-02-24 01:34:55 +0300

    updating my company

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav style={{ padding: "10px", background: "#333", color: "white" }}>
+    <nav style={{ padding: "10px", background: "#333", color: "white", backgroundColor: 'tomato', display:'flex', justifyContent:'center' }}>
       <Link to="/" style={{ margin: "10px", color: "white", textDecoration: "none" }}>Home</Link>
       <Link to="/about" style={{ margin: "10px", color: "white", textDecoration: "none" }}>About</Link>
       <Link to="/services" style={{ margin: "10px", color: "white", textDecoration: "none" }}>Services</Link>
