69581671c525b5a63610c7e6e6a5b034a8d8cfb1
Mwende Nzau <ancentamwende@gmail.com>
2025-02-23 23:45:44 +0300

    modiified props

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,11 +1,17 @@
-const UserProfile = (props) => {
-    return (
-      <div>
-        <h2>{props.name}</h2>
-        <p>Age: {props.age}</p>
-        <p>Bio: {props.bio}</p>
-      </div>
-    );
-  };
-  
-  export default UserProfile;
+
+import React, { useContext } from "react";
+import UserContext from "../context/UserContext"; // Import UserContext
+
+const UserProfile = () => {
+  const userData = useContext(UserContext); // Access user data from context
+
+  return (
+    <div>
+      <h2>{userData.name}</h2>
+      <p>Age: {userData.age}</p>
+      <p>Bio: {userData.bio}</p>
+    </div>
+  );
+};
+
+export default UserProfile;
