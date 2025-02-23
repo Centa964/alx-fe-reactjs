8adc9fac6a5f355d98f82d86fa07e0d4dde4cf23
Mwende Nzau <ancentamwende@gmail.com>
2025-02-23 23:41:37 +0300

    refactored a task

---
+++ alx-react-app-props/src/components/ProfilePage.jsx
@@ -0,0 +1,8 @@
+import UserInfo from './UserInfo';
+import UserInfo from './UserInfo';
+
+function ProfilePage({ userData }) {
+  return <UserInfo userData={userData} />;
+}
+
+export default ProfilePage;
