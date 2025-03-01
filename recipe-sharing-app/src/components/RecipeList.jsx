d956bb184986447a54c9dfd3c3da9274e6169cc8
Mwende Nzau <ancentamwende@gmail.com>
2025-03-01 23:51:20 +0300

    fixed a bug

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,17 +1,32 @@
-import useRecipeStore from './recipeStore'
+import useRecipeStore from "./recipeStore";
+import { Link } from "react-router-dom";
 
-  const RecipeList = () => {
-    const recipes = useRecipeStore(state => state.recipes);
+const RecipeList = () => {
+  const recipes = useRecipeStore((state) => state.recipes);
+  const filteredRecipes = useRecipeStore((state) => state.filteredRecipes);
+  const filterRecipes = useRecipeStore((state) => state.filterRecipes);
+  const searchTerm = useRecipeStore((state) => state.searchTerm);
 
-    return (
-      <div>
-        {recipes.map(recipe => (
+  useEffect(() => {
+    filterRecipes();
+  }, [searchTerm, filterRecipes]);
+
+  return (
+    <div>
+      {recipes.length === 0 ? (
+        <p>No recipes yet!</p>
+      ) : (
+        recipes.map((recipe) => (
           <div key={recipe.id}>
-            <h3>{recipe.title}</h3>
+            <h3>
+              <Link to={`/recipe/${recipe.id}`}>{recipe.title}</Link>
+            </h3>
             <p>{recipe.description}</p>
           </div>
-        ))}
-      </div>
-    );
-  };
-  export default RecipeList
+        ))
+      )}
+    </div>
+  );
+};
+
+export default RecipeList;
