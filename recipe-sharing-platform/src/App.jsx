11a166135b1fbb3dfa52d399c4351fea4f2a0a22
Mwende Nzau <ancentamwende@gmail.com>
2025-03-17 18:04:48 +0300

    build the homepage

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import HomePage from './components/HomePage'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,26 +10,9 @@ function App() {
     <div className='text-blue-500 text-center text-2xl'>
       <h1>Recipe Sharing Platform</h1>
     </div>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <div className='bg-gray-100 min-h-screen'>
+      <HomePage />
+     </div>
     </>
   )
 }
---
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+
+const HomePage = () => {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    fetch('/src/data.json')
+      .then((response) => response.json())
+      .then((data) => setRecipes(data))
+      .catch((error) => {
+        console.error('Error Loading Data:', error);
+      });
+  }, []);
+
+ return (
+    <div className="p-4">
+      <h1 className="font-semibold text-center text-3xl text-green-500">Recipe Sharing Platform</h1>
+      <ul className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {recipes.map((recipe) => (
+          <li key={recipe.id}>
+            <h2 className="text-xl font-medium mt-2">{recipe.name}</h2>
+            <p className="mt-1 text-green-950">{recipe.description}</p>
+            <img className="w-full h-40 object-cover rounded" src="recipe.image" alt="recipe.title" />
+          </li>
+        ))}
+      </ul>
+    </div>
+  )};
+
+export default HomePage;
