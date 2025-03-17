fe5f0aba423f46159e3a1ac2e5e6919b48495d38
Mwende Nzau <ancentamwende@gmail.com>
2025-03-17 18:21:40 +0300

    added hover:shadow

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -17,7 +17,7 @@ const HomePage = () => {
       <h1 className="font-semibold text-center text-3xl text-green-500">Recipe Sharing Platform</h1>
       <ul className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {recipes.map((recipe) => (
-          <li key={recipe.id}>
+          <li className="shadow-md hover:shadow-xl hover:scale-100 transition-transform duration-200" key={recipe.id}>
             <h2 className="text-xl font-medium mt-2">{recipe.name}</h2>
             <p className="mt-1 text-green-950">{recipe.description}</p>
             <img className="w-full h-40 object-cover rounded" src="recipe.image" alt="recipe.title" />
