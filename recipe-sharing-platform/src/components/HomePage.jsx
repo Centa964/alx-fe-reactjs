685273db4d1f39b635df12a90fae43faafc532af
Mwende Nzau <ancentamwende@gmail.com>
2025-03-17 19:31:57 +0300

    added link

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 const HomePage = () => {
   const [recipes, setRecipes] = useState([]);
@@ -12,19 +13,29 @@ const HomePage = () => {
       });
   }, []);
 
- return (
+  return (
     <div className="p-4">
       <h1 className="font-semibold text-center text-3xl text-green-500">Recipe Sharing Platform</h1>
       <ul className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {recipes.map((recipe) => (
-          <li className="shadow-md hover:shadow-xl hover:scale-100 transition-transform duration-200" key={recipe.id}>
-            <h2 className="text-xl font-medium mt-2">{recipe.name}</h2>
-            <p className="mt-1 text-green-950">{recipe.description}</p>
-            <img className="w-full h-40 object-cover rounded" src="recipe.image" alt="recipe.title" />
+          <li
+            className="shadow-md hover:shadow-xl hover:scale-100 transition-transform duration-200"
+            key={recipe.id}
+          >
+            <Link to={`/recipe/${recipe.id}`} className="block">
+              <h2 className="text-xl font-medium mt-2">{recipe.title}</h2>
+              <p className="mt-1 text-green-950">{recipe.summary}</p>
+              <img
+                className="w-full h-40 object-cover rounded"
+                src={recipe.image}
+                alt={recipe.title}
+              />
+            </Link>
           </li>
         ))}
       </ul>
     </div>
-  )};
+  );
+};
 
 export default HomePage;
